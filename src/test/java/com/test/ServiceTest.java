8159a6a527bb92c9c6b9894f95e94e69f59ab711
Sai Yugandhar22 <saiyugandhar59@gmail.com>
2025-03-11 14:33:06 +0530

    First Commit

---
+++ src/test/java/com/test/ServiceTest.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.Service;
+
+public class ServiceTest {
+	@Test
+	public void TestEmailOne() {
+		Service service=new Service();
+		assertTrue(service.validateEmail("abc@xyz.com"));
+	}
+	@Test
+	public void TestEmailTwo() {
+		Service service=new Service();
+		assertFalse(service.validateEmail("abcxyz.com"));
+	}
+}
